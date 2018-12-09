f90cb53dd696519abf8ec28c7cdb3a14cc1c03f5
Nikos Mouchtaris <nikolaos.mouchtaris@nike.com>
2018-12-09 10:43:22 +0000

    SMS first

--- sms/src/sms.cr
+++ sms/src/sms.cr
@@ -1,6 +1,70 @@
-# TODO: Write documentation for `Sms`
+# vim: et ts=2 sw=2
+
+module Args
+  def initialize(argv)
+    @argv = argv || [] of String
+    @argi = 0
+  end
+
+  def arg_shift
+    if @argi >= @argv.size
+      raise ArgumentError.new("Args done at #{@argi}")
+    end
+    @argv[@argi].tap do
+      @argi += 1
+    end
+  end
+end
+
 module Sms
   VERSION = "0.1.0"
+  include Args
 
-  # TODO: Put your code here
+  def dispatchoes
+    {
+      help: ->cli_help,
+      new: ->cli_new,
+    }
+  end
+
+  def news
+    {
+      fun: ->cli_new_fun
+    }
+  end
+
+  def cli_dispatcho
+    command = arg_shift
+    dispatchoes[command].call
+  end
+
+  def cli_new
+    wat = arg_shift
+    news[wat].call
+  end
+
+  def cli_help
+    puts "=== Commands ==="
+    pp dispatchoes.keys
+    puts ""
+    puts "--- new ---"
+    pp news.keys
+  end
+
+  def cli_new_fun
+    ns = arg_shift
+    name = arg_shift
+    puts "#{ns}/src/#{name}"
+  end
 end
+
+class Cli
+  include Sms
+
+  def initialize(*args)
+    super
+  end
+end
+
+cli = Cli.new(ARGV)
+cli.cli_dispatcho
