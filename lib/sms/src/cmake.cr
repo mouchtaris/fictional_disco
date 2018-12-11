1f273da317cbb5365e0b72886a5e007e16f80f2c
Nikos Mouchtaris <nikolaos.mouchtaris@nike.com>
2018-12-11 10:37:55 +0000

    CMake generation pt2

--- lib/sms/src/cmake.cr
+++ lib/sms/src/cmake.cr
@@ -8,21 +8,53 @@ module CMake
     @conf.subconf(:skeletor).subconf("CMakeLists.txt")
   end
 
+  def write_skel(name)
+    skeletor.list(name).each { |l| @io.puts l }
+  end
+
+  def write(line)
+    @io.puts line
+  end
+
+  def add_executable(name)
+    write "add_executable(#{name})"
+  end
+
+  def add_library(name)
+    write "add_library(#{name})"
+  end
+
+  def target_source(target, source)
+    write "target_sources(#{target} PRIVATE #{source})"
+  end
+
+  def target_link_libraries(target, libr, style = "PRIVATE")
+    write "target_link_libraries(#{target} #{style} #{libr})"
+  end
+
+  def target_compile_options(target)
+    target_link_libraries target, "_compile_options"
+  end
+
   def header
-    skeletor.list(:header).each { |l| @io.puts l }
-  end
-
-  def target_main
-    @io.puts "add_executable(main)"
-  end
-
-  def target_test
-    @io.puts "add_executable(test)"
+    write_skel :header
   end
 
   def call
     header
-    target_main
-    target_test
+
+    add_library :lib
+    target_compile_options :lib
+    target_source :lib, "src/lib.cpp"
+
+    add_executable :main
+    target_compile_options :main
+    target_source :main, "src/main.cpp"
+    target_link_libraries :main, :lib
+
+    add_executable :test
+    target_compile_options :test
+    target_source :test, "test/main.cpp"
+    target_link_libraries :test, :lib
   end
 end
