71a20ed244bef7c966cdd0a313aae712cd976c6b
Nikos Mouchtaris <nikolaos.mouchtaris@nike.com>
2018-12-27 21:56:11 +0000

    TPF test

--- test/test_tpf.cpp
+++ test/test_tpf.cpp
@@ -5,7 +5,19 @@ namespace
 {
     using namespace tpf;
     struct nothing;
+
+    struct int2float;
 }
+template <> struct def<int2float, int> { using result = float; };
+
 void test_tpf()
 {
+    static_assert(!is_defined<def<nothing>>::value, "");
+    static_assert(!is_defined<def<nothing, int>>::value, "");
+
+    static_assert(is_defined<def<int2float, int>>::value, "");
+    static_assert(!is_defined<def<int2float>>::value, "");
+    static_assert(!is_defined<def<int2float, float>>::value, "");
+
+    static_assert(std::is_same_v<eval<int2float, int>, float>, "");
 }
